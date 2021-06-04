6683e99778842badd6d360acb7927db32d9ec08d
baibhav-pixel <baibhav.sinha^@gmail.com>
2021-06-04 17:04:05 +0530

    ok with changes

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -10,18 +10,31 @@ namespace UnitTestProject1
         UserLogin obj = new UserLogin();
 
         [TestMethod]
-        [DataTestMethod]
-        [DataRow("Aam","aadmi")]
-        [DataRow("Mango","Man")]
-        [DataRow("admin","Admin123")]
-        [DataRow("Admin","admin123")]
-        [DataRow("Admin", "Admin123")]
-        public void TestUser(string user,string pass)
+        [DataRow("Admin", "admin123")]
+        public void TestUser(string user, string pass)
         {
-            bool output = obj.login(user, pass);
-            
-            Assert.AreEqual(true,output);
-            
+            bool actual = obj.login(user, pass);
+            bool expected = true;
+            Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("Aam", "aadmi")]
+        [DataRow("Mango", "Man")]
+        [DataRow("admin", "Admin123")]
+        [DataRow("Admin", "Admin123")]
+        public void TestWrongUserDetails(string user, string pass)
+        {
+            bool expected = false;
+            if (user == "Admin" && pass == "admin123")
+            {
+                expected = true;
+            }
+            bool actual = obj.login(user, pass);
+            Assert.AreEqual(expected, actual);
+        }
+    
+        
     }
 }
