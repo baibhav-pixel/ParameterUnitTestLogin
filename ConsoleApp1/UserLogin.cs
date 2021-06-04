f7e7ea15431e2daf0bf4d01852d6787e24336567
baibhav-pixel <baibhav.sinha^@gmail.com>
2021-06-04 16:25:30 +0530

    done with changes

---
+++ ConsoleApp1/UserLogin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class UserLogin
+    {
+        
+            public bool login(string username, string password)
+            {
+                if (username.Equals("Admin") && password.Equals("admin123"))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ConsoleApp1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        UserLogin obj = new UserLogin();
+
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow("Aam","aadmi")]
+        [DataRow("Mango","Man")]
+        [DataRow("admin","Admin123")]
+        [DataRow("Admin","admin123")]
+        [DataRow("Admin", "Admin123")]
+        public void TestUser(string user,string pass)
+        {
+            bool output = obj.login(user, pass);
+            
+            Assert.AreEqual(true,output);
+            
+        }
+    }
+}
